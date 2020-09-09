e88f37282238043de0bc58c90b6405bd2c30d600
Owais Ahmed Khan <owais1566@gmail.com>
2020-09-09 16:23:27 +0000

    update data

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
+  { id: 11, name: 'Daniyal Nagori' },
   { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
+  { id: 13, name: 'Owais Ahmed Khan' },
   { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
+  { id: 15, name: 'Mohsin Khalid' },
   { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
+  { id: 17, name: 'Saad Ahmed Khan' },
   { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
+  { id: 19, name: 'Aziz Rashid' },
   { id: 20, name: 'Tornado' }
 ];
